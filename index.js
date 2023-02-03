5a371068c08ddb6448354fc55aa2cfc5ee4b3bc5
Saurabh <saurabh.koli7420@gmail.com>
2023-02-03 12:16:33 +0530

    Created the Directory Structure and Setup the Express Server

---
+++ index.js
@@ -0,0 +1,31 @@
+//Import Express library
+const express = require('express');
+
+//Creating instance of express
+const app = express();
+
+//Initializing port
+const port = 8000;
+
+
+
+
+
+
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//Running the Server
+app.listen(port,function(err){
+
+    //If Error occures
+    if(err){
+        console.log(`Error in running the Server :${err} `);
+        return ;
+    }
+
+
+    //If Server executes Successfully then print the following
+    console.log(`Server is running on port: ${port}`);
+});
